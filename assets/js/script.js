ab5f4fd1baeea780cffd9cb6ea191c7db3102a05
David <david.jimenez.web@gmail.com>
2024-01-23 13:03:40 +0100

    Inicio preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,40 @@
-// * BUTTON NAV ANIMATION
-const menuBtn = document.querySelector('.dj-menu-btn');
-const menuNavbar = document.querySelector('.dj-menu-navbar');
-const menuBackdrop = document.querySelector('.dj-menu-backdrop');
-const menuContent  = document.querySelector('.dj-menu-content');
+document.addEventListener('DOMContentLoaded', () => {
 
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
+   // * HScroll
+   var width = window.innerWidth;
+   if(width >= 1079) {
 
-   if(!menuOpen) {
-
-      menuBtn.classList.add('open');
-      menuBackdrop.classList.add('dj-menu-active');
-      menuContent.classList.add('dj-menu-active');
-      menuNavbar.classList.add('open');
-      menuOpen = true;
-
-   } else {
-
-      menuBtn.classList.remove('open');
-      menuBackdrop.classList.remove('dj-menu-active');
-      menuContent.classList.remove('dj-menu-active');
-      menuNavbar.classList.remove('open');
-      menuOpen = false;
+      var s = skrollr.init();
 
    }
+
+   // * Button nav navigation
+   const menuBtn      = document.querySelector('.dj-menu-btn');
+   const menuNavbar   = document.querySelector('.dj-menu-navbar');
+   const menuBackdrop = document.querySelector('.dj-menu-backdrop');
+   const menuContent  = document.querySelector('.dj-menu-content');
+
+   let menuOpen = false;
+   menuBtn.addEventListener('click', () => {
+
+      if(!menuOpen) {
+
+         menuBtn.classList.add('open');
+         menuBackdrop.classList.add('dj-menu-active');
+         menuContent.classList.add('dj-menu-active');
+         menuNavbar.classList.add('open');
+         menuOpen = true;
+
+      } else {
+
+         menuBtn.classList.remove('open');
+         menuBackdrop.classList.remove('dj-menu-active');
+         menuContent.classList.remove('dj-menu-active');
+         menuNavbar.classList.remove('open');
+         menuOpen = false;
+
+      }
+
+   });
 
 });
