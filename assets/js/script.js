cfa275cd4b1cba5db110985bed44f7eeb737a507
David <david.jimenez.web@gmail.com>
2024-01-25 17:36:31 +0100

    Muchas correcciónes

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       document.addEventListener('scroll', () => {
 
-         if(window.scrollY > 50) {
+         if(window.scrollY > 100) {
 
             selectStart.classList.add('start-scrolled')
             selectIntro.classList.add('intro-scrolled')
@@ -90,79 +90,4 @@ document.addEventListener('DOMContentLoaded', () => {
       
    });
 
-   /* Barba Init
-   barba.init({
-      
-      transitions: [{
-
-         name: 'opacity-transition',
-         leave(data) {
-
-            return gsap.to(data.current.container, {
-
-               opacity: 0
-
-            });
-
-         },
-         enter(data) {
-
-            return gsap.from(data.next.container, {
-
-               opacity: 0
-
-            });
-
-         }
-
-      }]
-
-   });*/
-
 });
-
-/* function delay(n) {
-   n = n || 2000;
-   return new Promise((done) => {
-      setTimeout(() => {
-         done();
-      }, n);
-   });
-}
-
-function pageTransition() {
-   var tl = gsap.timeline();
-   tl.to(".loading-screen", {
-      duration: 1.2,
-      width: "100%",
-      left: "0%",
-      ease: "Expo.easeInOut",
-   });
-
-   tl.to(".loading-screen", {
-      duration: 1,
-      width: "100%",
-      left: "100%",
-      ease: "Expo.easeInOut",
-      delay: 0.3,
-   });
-   tl.set(".loading-screen", { left: "-100%" });
-}
-
-$(function () {
-   barba.init({
-       sync: true,
-
-       transitions: [
-         {
-            async leave(data) {
-               const done = this.async();
-
-               pageTransition();
-               await delay(1000);
-               done();
-            },
-         },
-      ],
-   });
-}); */
