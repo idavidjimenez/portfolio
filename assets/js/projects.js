09136a94a0dfdb863ca06dab08daf50810864a1a
David <david.jimenez.web@gmail.com>
2024-01-30 17:49:59 +0100

    Modal platform

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,41 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+   // * Obtener todos los elementos con la clase 'dj-selector-tab'
    const selectorTabs = document.querySelectorAll('.dj-selector-tab');
 
-   // * Function to change the active tab
-   function changeActiveTab(index) {
-      
-      // * Remove 'active' class from all 'dj-selector-tab' elements
-      selectorTabs.forEach(tab => { tab.classList.remove('active'); });
-
-      // * Add the class 'active' to the current element
-      selectorTabs[index].classList.add('active');
-
-      // * Remove the 'active' class from all 'dj-content-tabs-item' elements.
-      const contentItems = document.querySelectorAll('.dj-content-tabs-item');
-      contentItems.forEach(item => { item.classList.remove('active'); });
-
-      // * Add the class 'active' to the element corresponding to the current index
-      const contentToShow = document.querySelectorAll('.dj-content-tabs-item')[index];
-      contentToShow.classList.add('active');
-
-   }
-
-   // *Event listener for tabs
+   // * Iterar sobre cada elemento y agregar un event listener
    selectorTabs.forEach((tab, index) => {
 
       tab.addEventListener('click', () => {
 
-         // * Clicking on a tab saves the index to localStorage
-         localStorage.setItem('activeTabIndex', index);
-         changeActiveTab(index);
+         // * Remover la clase 'active' de todos los elementos 'dj-selector-tab'
+         selectorTabs.forEach(tab => {
+
+            tab.classList.remove('active');
+
+         });
+
+         // * Agregar la clase 'active' al elemento actual
+         tab.classList.add('active');
+
+         // * Remover la clase 'active' de todos los elementos 'dj-content-tabs-item'
+         const contentItems = document.querySelectorAll('.dj-content-tabs-item');
+         contentItems.forEach(item => {
+
+            item.classList.remove('active');
+         
+         });
+
+         // * Agregar la clase 'active' al elemento correspondiente al índice actual
+         const contentToShow = document.querySelectorAll('.dj-content-tabs-item')[index];
+         contentToShow.classList.add('active');
 
       });
 
    });
-
-   // * On page load, check if there is an active index on localStorage
-   const activeTabIndex = localStorage.getItem('activeTabIndex');
-   if (activeTabIndex !== null) { changeActiveTab(parseInt(activeTabIndex)); }
 
 });
